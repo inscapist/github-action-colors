8fb5d8c02f4e16a6353ba50a5662421caac573e1
Felix <zen9.felix@gmail.com>
2022-11-04 10:06:43 +0800

    Print in columns

--- term-colors.sh
+++ term-colors.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-end=$(( $(tput colors)-1 ))
+end=$(($(tput colors) - 1))
+w=8
+i=0
 for fg in $(seq 0 $end); do
     for bg in $(seq 0 $end); do
         set_fg=$(tput setaf $fg)
         set_bg=$(tput setab $bg)
         reset=$(tput sgr0)
-        printf "${set_fg}${set_bg}F:%s B:%s${reset}\n" $fg $bg
+        printf "${set_fg}${set_bg}F:%-3s B:%-3s${reset}" $fg $bg
+        ((i++))
+        [[ $((i % w)) == $((w - 1)) ]] && echo
     done
 done
-
